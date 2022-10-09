54cef5e35fbb8624b51851c8c7349ee943a5ec86
Anton <anton.sokolov87@gmail.com>
2022-10-09 23:31:05 +0300

    remove AuthProvider add to SecurityConfig PersonService for authentication

--- src/main/java/ru/sokolov/Test_SpringBootSecurity/config/SecurityConfig.java
+++ src/main/java/ru/sokolov/Test_SpringBootSecurity/config/SecurityConfig.java
@@ -1,25 +1,33 @@
 package ru.sokolov.Test_SpringBootSecurity.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import ru.sokolov.Test_SpringBootSecurity.security.AuthProviderImpl;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.sokolov.Test_SpringBootSecurity.services.PersonService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
+    private final PersonService personService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonService personService) {
+        this.personService = personService;
     }
 
     //Настраивает аунтификацию
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authProvider);
+        auth.userDetailsService(personService);
     }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 
 }
