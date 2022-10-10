eb3db740318ef773527ad3115d12e1b2ab2c9264
Anton <anton.sokolov87@gmail.com>
2022-10-10 21:16:31 +0300

    add custom login page change SecurityConfig

--- src/main/java/ru/sokolov/Test_SpringBootSecurity/config/SecurityConfig.java
+++ src/main/java/ru/sokolov/Test_SpringBootSecurity/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package ru.sokolov.Test_SpringBootSecurity.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -19,7 +20,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.personService = personService;
     }
 
+
+    //настройка авторизации, страницы логина
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .csrf().disable()   //  отключение защиты от межсайтовой подделки запросов
+                .authorizeRequests()
+                .antMatchers("/auth/login","/error").permitAll()   // разрешенные странички всем
+                .anyRequest().authenticated()   // все остальные доступны только прошедшим авторизацию
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login") // метод который приходит из POST формы с вводом логина и пароля
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/auth/login?error");// при неправильном логине или пароле отправит на страницу логина с параметром error
+    }
+
     //Настраивает аунтификацию
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personService);
     }
